c9df8ad748bf4973fa93d27a82d42a7ed2968b86
ClauS-Mkt <clauber@mundodeled.com.br>
2025-07-03 10:28:44 -0300

    Update firebase-config.js

---
+++ js/firebase-config.js
@@ -0,0 +1,20 @@
+// firebase-config.js
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.10.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/11.10.0/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/11.10.0/firebase-firestore.js";
+
+// Configuração
+const firebaseConfig = {
+  apiKey: "AIzaSyBs1jwFTLVKTZrmviCjuC70__A2Q-VGLog",
+  authDomain: "projecao-c1ffc.firebaseapp.com",
+  projectId: "projecao-c1ffc",
+  storageBucket: "projecao-c1ffc.firebasestorage.app",
+  messagingSenderId: "1048500488131",
+  appId: "1:1048500488131:web:1b6a40e4268eac8f819f76"
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
+
+export { auth, db };
